89d4dfc3869132ba9108507e47c2e8013fcbba37
YOUR NAME <jhanov@gmail.com>
2021-08-02 09:06:31 -0500

    make index page and import data from axios

---
+++ src/views/SpotsIndex.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+
+    <p> {{ spots }}</p>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Find a spot",
+      spots: [],
+    };
+  },
+  created: function () {
+    this.spotsIndex();
+  },
+  methods: {
+    spotsIndex: function () {
+      console.log("Showing all spots...");
+      axios.get("/spots").then((response) => {
+        console.log("spots index", response.data);
+        this.spots = response.data;
+      });
+    },
+  },
+};
+</script>
